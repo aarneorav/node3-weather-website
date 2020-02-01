d8dd5d54d88e22af7ac9207da2629f9b460b17a1
Aarne Orav <kurirock@gmail.com>
2020-02-01 12:25:25 +0200

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,11 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.  ', undefined)
         } else {
-            callback(undefined, {
-                summary: body.daily.data[0].summary,
-                temperature: body.currently.temperature,
-                precipProbability: body.currently.precipProbability
-            })
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a lof of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + ' % chance of rain'
+            )
         }
     })
 }
